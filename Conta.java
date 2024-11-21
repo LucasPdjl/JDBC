004b4dee2a01df68503eaf688d6b098a14aabf09
LucasPdjl <168298658+LucasPdjl@users.noreply.github.com>
2024-11-20 22:02:49 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Conta {
-    private String numero; // Número da conta
-    private double saldo;  // Saldo da conta
-    private Agencia agencia; // Agência associada à conta
-    private List<Cliente> clientes; // Lista de clientes associados à conta
+    private String numero;
+    private double saldo;
+    private Agencia agencia;
+    private List<Cliente> clientes;
 
     public Conta(String numero, double saldo, Agencia agencia) {
         this.numero = numero;
@@ -14,42 +14,34 @@ public class Conta {
         this.clientes = new ArrayList<>();
     }
 
-    // Getter para saldo
+
     public double getSaldo() {
         return saldo;
     }
 
-    // Setter para saldo
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    // Getter para número da conta
     public String getNumero() {
         return numero;
     }
-
-    // Setter para número da conta
     public void setNumero(String numero) {
         this.numero = numero;
     }
 
-    // Getter para agência
     public Agencia getAgencia() {
         return agencia;
     }
 
-    // Setter para agência
     public void setAgencia(Agencia agencia) {
         this.agencia = agencia;
     }
 
-    // Adicionar cliente à conta
     public void adicionarCliente(Cliente cliente) {
         this.clientes.add(cliente);
     }
 
-    // Getter para clientes
     public List<Cliente> getClientes() {
         return clientes;
     }
